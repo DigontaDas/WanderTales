8cb193ba05503358da4349e0de7514207ccf27bd
Digonta Das <118357100+DigontaDas@users.noreply.github.com>
2025-08-16 23:48:22 +0600

    Update App.jsx

--- View/App.jsx
+++ View/App.jsx
@@ -6,11 +6,14 @@ import HomePage from "./pages/HomePage";
 import Stories from "./pages/Stories";
 import Destinations from "./pages/Destinations";
 import Admin from "./pages/Admin"
-
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
-import StoryDetailPage from "./pages/StoryDetailPage";
+import StoryDetail from "./pages/StoryDetail";
+import HotelRestaurants from "./pages/HotelRestaurants";
+import HotelRestaurantDetail from "./pages/HotelRestaurantDetail";
+import Addreview from "./pages/Addreview";
+import UserProfile from "./pages/UserProfile";
 export default function App() {
   return (
     <>
@@ -21,9 +24,14 @@ export default function App() {
       <Route path="/destinations" element={<Destinations/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
-      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/profile" element={<UserProfile/>}/>
+      <Route path="/user/:userId" element={<UserProfile/>}/>
       <Route path="/admin" element={<Admin/>}/>
-      <Route path="/storydetail" element={<StoryDetailPage/>}/>
+      <Route path="/story/:id" element={<StoryDetail/>}/>
+      <Route path="/hotelrestaurants" element={<HotelRestaurants/>}/>
+      <Route path="/hotel/:id" element={<HotelRestaurantDetail/>}/>
+      <Route path="/restaurant/:id" element={<HotelRestaurantDetail/>}/>
+      <Route path="/addreview" element={<Addreview/>}/>
       </Routes>
       <Footer />
     </>
