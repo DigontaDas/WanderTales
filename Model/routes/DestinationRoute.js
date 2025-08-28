a4418356d68675473096a02be54386da49a6d882
Digonta Das <118357100+DigontaDas@users.noreply.github.com>
2025-08-29 00:01:05 +0600

    Update DestinationRoute.js

--- Model/routes/DestinationRoute.js
+++ Model/routes/DestinationRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import {addDestination,listDestination,removeDestination,singleDestination} from "../Controller/destinationController.js"
 import upload from "../middleware/multer.js"
+import adminAuth from "../middleware/adminAuth.js"
 
 const DestinationRouter=express.Router()
-DestinationRouter.post("/add",upload.fields([{name:"image1",maxCount:1}]),addDestination)
+DestinationRouter.post("/add",adminAuth,upload.fields([{name:"image1",maxCount:1}]),addDestination)
 DestinationRouter.post("/remove",removeDestination)
 DestinationRouter.post("/single",singleDestination)
 DestinationRouter.get("/list",listDestination)
