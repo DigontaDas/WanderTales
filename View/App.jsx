7143de45a2f9ef83805ba2e69aad11572c79e8ee
Digonta Das <118357100+DigontaDas@users.noreply.github.com>
2025-08-29 00:08:23 +0600

    Update App.jsx

--- View/App.jsx
+++ View/App.jsx
@@ -1,37 +1,39 @@
 import React from "react";
-import { Route,Routes } from "react-router";
+import { Route,Routes,useLocation } from "react-router";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage";
 import Stories from "./pages/Stories";
 import Destinations from "./pages/Destinations";
-import Admin from "./pages/Admin"
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
 import StoryDetail from "./pages/StoryDetail";
 import HotelRestaurants from "./pages/HotelRestaurants";
 import HotelRestaurantDetail from "./pages/HotelRestaurantDetail";
 import Addreview from "./pages/Addreview";
 import UserProfile from "./pages/UserProfile";
+import AdminPanel from "./pages/AdminPanel";
+import AdminHeader from "./components/Header/AdminHeader";
+
 export default function App() {
+  const location = useLocation();
   return (
     <>
-      <Header/>
+      {location.pathname === "/admin" ? <AdminHeader /> : <Header />}
       <Routes>
       <Route path="/" element={<HomePage/>}/>
-      <Route path="/stories" element={<Stories/>}/> //add id in stories/:id
+      <Route path="/stories" element={<Stories/>}/> 
       <Route path="/destinations" element={<Destinations/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/profile" element={<UserProfile/>}/>
       <Route path="/user/:userId" element={<UserProfile/>}/>
-      <Route path="/admin" element={<Admin/>}/>
       <Route path="/story/:id" element={<StoryDetail/>}/>
-      <Route path="/hotelrestaurants" element={<HotelRestaurants/>}/>
+      <Route path="/hotel_restaurants" element={<HotelRestaurants/>}/>
       <Route path="/hotel/:id" element={<HotelRestaurantDetail/>}/>
       <Route path="/restaurant/:id" element={<HotelRestaurantDetail/>}/>
       <Route path="/addreview" element={<Addreview/>}/>
+      <Route path="/admin" element={<AdminPanel/>}/>
       </Routes>
       <Footer />
     </>
