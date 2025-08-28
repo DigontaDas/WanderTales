1878274e83dfad43eb1a243dadb9200ae5d0a0b2
Digonta Das <118357100+DigontaDas@users.noreply.github.com>
2025-08-29 00:01:29 +0600

    Update userRoute.js

--- Model/routes/userRoute.js
+++ Model/routes/userRoute.js
@@ -1,8 +1,37 @@
 import express from 'express'
-import { loginUser,registerUser,adminLogin } from '../Controller/userController.js'
+import { 
+    loginUser,
+  registerUser,
+  getUserBookmarks,
+  getUserProfile,
+  updateUserProfile,
+  getUserStories,
+  getUserReviews,
+  followUser,
+  getFollowStatus,
+  followModel,
+  getAllUsers,
+  deleteUser
+} from '../Controller/userController.js'
+import adminAuth from '../middleware/adminAuth.js';
 
-const userRouter=express.Router();
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
-userRouter.post('/admin',adminLogin)
+const userRouter = express.Router();
+
+// Authentication routes
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+//userRouter.post('/admin', adminLogin)
+
+// Profile management routes
+userRouter.post('/profile', getUserProfile)
+userRouter.post('/update-profile', updateUserProfile)
+userRouter.post('/stories', getUserStories)
+userRouter.post('/reviews', getUserReviews)
+userRouter.post('/follow', followUser)
+userRouter.post('/follow-status', getFollowStatus)
+userRouter.get('/profile/:userId', getUserProfile)
+userRouter.post('/bookmarks', getUserBookmarks)
+//admin
+userRouter.get('/admin/users', adminAuth, getAllUsers);
+userRouter.delete('/admin/user', adminAuth, deleteUser);
 export default userRouter
