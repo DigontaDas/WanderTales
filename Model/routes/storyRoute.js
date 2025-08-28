509937d2c3b8afbe7dc2ca02304c0704af626062
Digonta Das <118357100+DigontaDas@users.noreply.github.com>
2025-08-29 00:01:20 +0600

    Update storyRoute.js

--- Model/routes/storyRoute.js
+++ Model/routes/storyRoute.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { addStory, listStories, getSingleStory, likeStory, removeStory } from "../Controller/storiesController.js";
+import { addComment, getComments, likeComment, deleteComment } from "../Controller/commentController.js";
 import upload from "../middleware/multer.js";
-
+import { toggleBookmark, getBookmarkStatus } from "../Controller/bookmarkController.js";
 const storiesRouter = express.Router();
 
+// story routes
 storiesRouter.post("/add", upload.fields([{ name: "image", maxCount: 1 }]), addStory);
 storiesRouter.get("/list", listStories);
 storiesRouter.post("/single", getSingleStory);
 storiesRouter.post("/like", likeStory);
 storiesRouter.post("/remove", removeStory);
-
+storiesRouter.post("/bookmark-status", getBookmarkStatus);
+// comment routes
+storiesRouter.post("/comments/add", addComment);
+storiesRouter.post("/comments/get", getComments);
+storiesRouter.post("/comments/like", likeComment);
+storiesRouter.post("/comments/delete", deleteComment);
+storiesRouter.post("/bookmark", toggleBookmark);
 export default storiesRouter;
