a638fc4bfb04040fd8b0f54095677e8aaa088b92
Digonta Das <118357100+DigontaDas@users.noreply.github.com>
2026-02-04 15:00:14 +0600

    Update server.js

--- Model/server.js
+++ Model/server.js
@@ -25,7 +25,8 @@ app.use(cors({
     'http://localhost:5173', 
     'http://localhost:3000',
     'http://127.0.0.1:5173',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://WanderTales.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
